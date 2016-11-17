ac52651dee1f094314120e13147bfa8eeec43ad1
ETi <avalokitesvara@ya.ru>
2016-11-17 19:00:04 +0300

    делаем выборку тэгов

--- dz4/04/4.js
+++ dz4/04/4.js
@@ -1,5 +1,23 @@
-var scanDOM = function(id){
-	var a;
-	
-	console.log(" ");
+var scanDOM = function(){
+	var page = document.getElementsByTagName('body'),
+		tags = page[0].getElementsByTagName('*'),
+		tstat = [];
+
+	// проходим по всем тэгам
+	for(let i=0; i<tags.length; i++){
+		let tag = tags[i].tagName;
+		// проверяем тэг на уникальность
+		var flag = 0;
+		for(let item of tstat){
+			if(item == tag){
+				flag = 1;
+				break;
+			}
+		}
+		// добавляем тэг в массив уникальных тэгов
+		if(!flag) tstat.push(tag);
+	}
+
+	console.log(tstat);
+
 }
