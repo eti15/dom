eb57461c8f638c1196f309a848fa3e73e978f9b5
ETi <avalokitesvara@ya.ru>
2016-11-17 15:52:57 +0300

    в процессе

--- dz4/02/2.js
+++ dz4/02/2.js
@@ -1,4 +1,9 @@
 var deleteTextNodes = function(id){
 	var con = document.getElementById(id);
 	
+	var elements = con.childNodes;
+	for(var item of elements){
+		console.log(item, item.nodeType);
+		//if(item.nodeType == )
+	}
 }
