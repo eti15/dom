3453aa12f4b81f5cc761ee445ee6b3da4a6491a3
ETi <avalokitesvara@ya.ru>
2016-11-17 17:27:34 +0300

    переделанная функция

--- dz4/03/3.js
+++ dz4/03/3.js
@@ -1,14 +1,15 @@
-var deleteTextNodesRec = function(id){
-	var con = document.getElementById(id),
-		elements = con.childNodes;
+var deleteTextNodesRec = function(block){
+	var elements = block.childNodes;
 	
 	console.log("Исходные дочерние узлы с их содержимым:");
 	for(var item of elements) console.log(item);
 
 	for(var item of elements){
-		//if(item.nodeType == 3) con.removeChild(item);
+		if(item.nodeType == 3) con.removeChild(item);
+		else if(item.nodeType == 1) deleteTextNodesRec(item);
 	}
 	
+	console.log("========================");
 	console.log("Оставшиеся узлы с их содержимым:");
 	for(var item of elements) console.log(item);
 }
