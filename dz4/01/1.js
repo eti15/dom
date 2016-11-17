13d135fded234ebdc79e2c6e41d0406eb5b1f430
ETi <avalokitesvara@ya.ru>
2016-11-17 14:17:24 +0300

    можно начинать

--- dz4/01/1.js
+++ dz4/01/1.js
@@ -1,2 +1,8 @@
-var a = "DZ4";
-console.log(a);
+var container = document.getElementById("container"),
+	new = "<li>Пункт 0</li>";
+
+var prepend = function(con, a){
+
+}
+
+prepend(container, new);
