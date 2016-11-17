300577320f2c8f4c8d176d479cc7ee61007a4276
ETi <avalokitesvara@ya.ru>
2016-11-17 16:05:27 +0300

    итоговый вариант

--- dz4/02/2.js
+++ dz4/02/2.js
@@ -2,8 +2,12 @@ var deleteTextNodes = function(id){
 	var con = document.getElementById(id);
 	
 	var elements = con.childNodes;
+	console.log("Исходные дочерние узлы:", elements);
+
 	for(var item of elements){
-		console.log(item, item.nodeType);
-		//if(item.nodeType == )
+		if(item.nodeType == 3) con.removeChild(item);
 	}
+	
+	console.log("Узлы, оставшиеся после удаления текстовых:", elements);
+	
 }
