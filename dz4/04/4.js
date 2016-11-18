9682efe07fab2b3641a1d06facf8db0b11352d73
ETi <avalokitesvara@ya.ru>
2016-11-18 23:52:40 +0300

    всё работает

--- dz4/04/4.js
+++ dz4/04/4.js
@@ -3,7 +3,7 @@ var tn = 0;
 var scanDOM = function(){
 	var page = document.getElementsByTagName('body'),	// элемент body
 		tags = page[0].getElementsByTagName('*'),		// все тэги
-		tstat = [];										// массив для статистики
+		tstat = [], cstat = [];							// массивы для статистики
 
 	// проходим по всем тэгам
 	for(let i=0; i<tags.length; i++){
@@ -18,12 +18,25 @@ var scanDOM = function(){
 		}
 		// добавляем тэг в массив уникальных тэгов
 		if(!flag) tstat.push(tag);
+
+		// проверяем классы
+		let styles = tags[i].classList;
+		if(!styles.length) continue;
+		
+		for(let item of styles){
+			flag = 0;
+			for(let cl of cstat){
+				if(item == cl) flag = 1;
+			}
+			// добавляем класс в массив уникальных классов
+			if(!flag) cstat.push(item);
+		}
 	}
 
-	console.log(tstat);
-	console.log('-------------');
-	console.log('Статистика по тэгам:');
+	//console.log(tstat);
+	//console.log('-------------');
 
+	// статистика по тэгам
 	for(let itemU of tstat){
 		let i = 0;
 		for(let item of tags){
@@ -32,12 +45,27 @@ var scanDOM = function(){
 		console.log('Тэгов', itemU, i);
 	}
 
-	// подсчёт текстовых узлов
+	// подсчёт всех текстовых узлов в body
 	textNodesCount(page[0]);
+	// статистика по текстовым узлам
 	console.log('Текстовых узлов:', tn);
-	//console.log(all);
+
+	// вывод статистики по классам
+	for(let cl of cstat){
+		let i = 0;
+		for(let tg of tags){
+			for(let c of tg.classList){
+				if(c == cl){
+					 i++;
+					 break;
+				}
+			}
+		}
+		console.log('Элементов с классом', cl, ':', i);
+	}
 }
 
+// функция подсчёта текстовых узлов
 var textNodesCount = function(block){
 	var all = block.childNodes;	// все узлы элемента на уровне корня
 	for(let a of all){
