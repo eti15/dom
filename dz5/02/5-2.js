9874ffa04f045c54fe16f4f8434b4f5eec0ecb3c
ETi <avalokitesvara@ya.ru>
2016-11-20 18:07:10 +0300

    произвольный див по клику - готов

--- dz5/02/5-2.js
+++ dz5/02/5-2.js
@@ -7,14 +7,24 @@ var listenToMe = function(bu){
 var tvorenie = function(){
 	var div1 = document.createElement('div'),
 		min = 1,
-		max = 840,
-		max2 = 480;
+		max = window.screen.width,
+		max2 = window.screen.height;
 
 	telo.appendChild(div1);
 
-	div1.style.width = '100px';
-	div1.style.height = '100px';
-	div1.style.marginBottom = '10px';
-	div1.style.backgroundColor = 'black';
-	//console.log('Да будет свет!');
+	var x1 = Math.floor(Math.random() * (max - 99 - min)) + min,
+		x2 = Math.floor(Math.random() * (max - min + 101)) + min,
+		y1 = Math.floor(Math.random() * (max2 - 99 - min)) + min,
+		y2 = Math.floor(Math.random() * (max2 - min + 101)) + min,
+		c1 = Math.floor(Math.random() * (256)),
+		c2 = Math.floor(Math.random() * (256)),
+		c3 = Math.floor(Math.random() * (256));
+
+	div1.style.position = 'absolute';
+	div1.style.left = x1 + 'px';
+	div1.style.top = y1 + 'px';
+	div1.style.width = x2 + 'px';
+	div1.style.height = y2 + 'px';
+	div1.style.backgroundColor = 'rgb(' + c1 + ',' + c2 + ',' + c3 + ')';
+	//console.log(div1);
 }
