ec0ab2edfac38cd8b2fcb49b3fdc67109783ce90
ETi <avalokitesvara@ya.ru>
2016-11-20 17:19:45 +0300

    творение чёрного квадрата

--- dz5/02/5-2.js
+++ dz5/02/5-2.js
@@ -5,5 +5,16 @@ var listenToMe = function(bu){
 }
 
 var tvorenie = function(){
-	console.log('Да будет свет!');
+	var div1 = document.createElement('div'),
+		min = 1,
+		max = 840,
+		max2 = 480;
+
+	telo.appendChild(div1);
+
+	div1.style.width = '100px';
+	div1.style.height = '100px';
+	div1.style.marginBottom = '10px';
+	div1.style.backgroundColor = 'black';
+	//console.log('Да будет свет!');
 }
