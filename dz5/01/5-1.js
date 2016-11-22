e7dcd1e50eece6b8ab99ead0f1fbf77225110595
ETi <avalokitesvara@ya.ru>
2016-11-22 16:21:38 +0300

    всё работает - и с закрыванием других пунктов

--- dz5/01/5-1.js
+++ dz5/01/5-1.js
@@ -1,8 +1,18 @@
 var opendiv = function(e){
 	var titlediv = e.target.parentNode;
-	if(titlediv.classList.contains('title')){
-		e.target.classList.toggle('title_a');
+	if(!titlediv.classList.contains('title')) return;
+	
+	e.target.classList.toggle('title_a');
+	
+	if(titlediv.nextElementSibling.classList.contains('text')){
+		current = null;
 		titlediv.nextElementSibling.classList.toggle('text');
-		console.log(titlediv.nextElementSibling);
+	} else {
+		if(current){
+			current.nextElementSibling.classList.toggle('text');
+			current.childNodes[1].classList.toggle('title_a');
+		}
+		current = titlediv;
+		current.nextElementSibling.classList.toggle('text');
 	}
 }
