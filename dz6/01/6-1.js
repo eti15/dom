7550f4b5e072459a7b841222d4c0588ebb54d6c5
ETi <avalokitesvara@ya.ru>
2016-11-26 21:39:08 +0300

    ошибки исправлены, всё работает

--- dz6/01/6-1.js
+++ dz6/01/6-1.js
@@ -1,16 +1,16 @@
-var time = 3000;
+var time = 2000,	// задержка
+	p;	// промис
 
 var timer = function(t){
-	var p = new Promise(function(resolve, reject){
+	p = new Promise(function(resolve, reject){
 	// асинхронный код
 	setTimeout(function(){
 		resolve();  // перевести промис в состояние "выполнено успешно"
 	},t);
-}
+})};
 
 timer(time);
 
 p.then(
-function(){console.log('выведено через', time/1000, 'секунд');},	// выполнить, если успешно (resolve)
-function(){console.log('что-то пошло не так');}		// выполнить, если неуспешно (reject)
-);	// выполняется в случае выполнения промиса
+function(){console.log('выведено через', time/1000, 'сек');}	// выполнить, если успешно (resolve)
+);
