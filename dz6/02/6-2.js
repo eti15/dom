869feb088b9a5d633e379e3b00136186c200905e
ETi <avalokitesvara@ya.ru>
2016-11-26 23:50:23 +0300

    готово

--- dz6/02/6-2.js
+++ dz6/02/6-2.js
@@ -17,13 +17,19 @@ var getCities = function(url){
 
 p = getCities('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
 
+// создаём нумерованный список для городов
+var list = document.createElement('ol');
+document.body.appendChild(list);
+
 p.then(
 	function(result){
 		cities = JSON.parse(result);	// ответ сервера переводим в массив объектов
-		for(let i=0; i<cities.length; i++){
-			cities[i] = cities[i].name;
-			console.log(cities[i]);
+		for(let i=0; i<cities.length; i++) cities[i] = cities[i].name;
+		cities.sort();
+		for(let i=0; i<cities.length; i++){	// помещаем города в элементы списка
+			let li = document.createElement('li');
+			li.textContent = cities[i];
+			list.appendChild(li);
 		}
-		//console.log(cities);
 	}
 );
