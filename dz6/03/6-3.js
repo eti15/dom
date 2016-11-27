cc08b65fcd0da147968964534fb89970d2f4f597
ETi <avalokitesvara@ya.ru>
2016-11-27 17:45:56 +0300

    исходная позиция

---
+++ dz6/03/6-3.js
@@ -0,0 +1,34 @@
+var cities = [],	// массив городов
+	p;	// промис
+
+var getCities = function(url){
+	return new Promise(function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.send();
+		xhr.addEventListener('load', function(){
+			resolve(xhr.response);
+		});
+		xhr.addEventListener('error', function(){
+			reject();
+		});
+	});
+}
+
+window.addEventListener('load', function(){
+	p = getCities('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+});
+
+p.then(
+	function(result){
+		cities = JSON.parse(result);	// ответ сервера переводим в массив объектов
+		for(let i=0; i<cities.length; i++) cities[i] = cities[i].name;
+		cities.sort();
+		/*for(let i=0; i<cities.length; i++){	// помещаем города в элементы списка
+			let li = document.createElement('li');
+			li.textContent = cities[i];
+			list.appendChild(li);
+		}*/
+	}
+);
+
