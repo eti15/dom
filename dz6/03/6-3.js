c7b54fdc7990ddc72c672c53a5f6cc37454c114d
ETi <avalokitesvara@ya.ru>
2016-11-27 18:19:35 +0300

    исправляем ошибки - обработка события input

--- dz6/03/6-3.js
+++ dz6/03/6-3.js
@@ -1,5 +1,6 @@
 var cities = [],	// массив городов
-	p;	// промис
+	p,	// промис
+	txt_in;	// input
 
 var getCities = function(url){
 	return new Promise(function(resolve, reject){
@@ -17,18 +18,23 @@ var getCities = function(url){
 
 window.addEventListener('load', function(){
 	p = getCities('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+
+	p.then(
+		function(result){
+			cities = JSON.parse(result);	// ответ сервера переводим в массив объектов
+			for(let i=0; i<cities.length; i++) cities[i] = cities[i].name;
+			cities.sort();
+		}
+	);
+
+	txt_in = document.getElementById('txt_in');
+	txt_in.addEventListener('input',function(){
+		console.log(cities);
+	});
 });
 
-p.then(
-	function(result){
-		cities = JSON.parse(result);	// ответ сервера переводим в массив объектов
-		for(let i=0; i<cities.length; i++) cities[i] = cities[i].name;
-		cities.sort();
 		/*for(let i=0; i<cities.length; i++){	// помещаем города в элементы списка
 			let li = document.createElement('li');
 			li.textContent = cities[i];
 			list.appendChild(li);
 		}*/
-	}
-);
-
