1061282338ee9dcc2beed1e0c753803e5c8a9fb4
ETi <avalokitesvara@ya.ru>
2016-11-29 16:35:34 +0300

    пишем значения кук в 2-мерный массив

--- dz7/01/7-1.js
+++ dz7/01/7-1.js
@@ -1,7 +1,20 @@
+var cookies,	// строка с куками
+	carr;	// массив для значений кук
+
 console.log('Куки до', document.cookie);
 
 document.cookie = "user=eti";
 document.cookie = "m=true";
 document.cookie = "city=Москва";
 
-console.log('Куки после', document.cookie);
+
+cookies = document.cookie;
+console.log('Куки после', cookies);
+
+carr = cookies.split('; ');
+console.log(carr);
+
+for(let i=0; i<carr.length; i++){
+	carr[i] = carr[i].split('=');
+}
+console.log(carr);
