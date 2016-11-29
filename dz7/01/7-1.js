9a44698b752ccc84d5abda6cd86aef2129520e66
ETi <avalokitesvara@ya.ru>
2016-11-29 17:10:07 +0300

    выводим куки с кнопками в таблицу

--- dz7/01/7-1.js
+++ dz7/01/7-1.js
@@ -7,14 +7,31 @@ document.cookie = "user=eti";
 document.cookie = "m=true";
 document.cookie = "city=Москва";
 
-
 cookies = document.cookie;
 console.log('Куки после', cookies);
 
-carr = cookies.split('; ');
-console.log(carr);
+
+carr = cookies.split('; ');	// 1-мерный массив с куками
+//console.log(carr);
+
+var cootab = document.getElementById('cootab');
 
 for(let i=0; i<carr.length; i++){
-	carr[i] = carr[i].split('=');
+	let newTr = document.createElement('tr');
+	carr[i] = carr[i].split('=');	// 2-мерный массив с куками
+	for(let j=0; j<3; j++){
+		let newTd = document.createElement('td');
+		if(j!=2) newTd.textContent = carr[i][j];
+		else {
+			let newBut = document.createElement('button');
+			newBut.textContent = "Удалить";
+			newBut.setAttribute('number', i);
+			newTd.appendChild(newBut);
+		}
+		newTr.appendChild(newTd);
+	}
+	cootab.appendChild(newTr);
 }
-console.log(carr);
+
+
+
