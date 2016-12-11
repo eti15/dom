183b3e2262ec13036421476e050bcec47ecc1c03
ETi <avalokitesvara@ya.ru>
2016-12-11 23:22:14 +0300

    реализована сортировка по дню рождения

--- dz8/02/8-2.js
+++ dz8/02/8-2.js
@@ -2,7 +2,8 @@ var headerInfo = document.getElementById('headerInfo'),
 	trList = document.getElementById('trList'),
 	shab,
 	shabFun,	// функция-компилятор Handlebars
-	today = new Date();
+	today = new Date(),	// текущая дата
+	friends = [];	// массив для отсортированных данных фрэндов
 
 
 new Promise(function(resolve) {
@@ -49,22 +50,48 @@ new Promise(function(resolve) {
         var len = response.response.length;
         for(let i=0; i<len; i++){
         	let item = response.response[i];
-        	if(item.bdate){
+        	if(item.bdate){		// если указана дата рождения
         		let d3 = item.bdate.split('.');
-        		if(d3[2]){
+        		if(d3[2]){		// если указан или не скрыт год рождения
         			let dateObj = new Date(d3[2], d3[1]-1, d3[0]);
         			dateObj = today - dateObj;
         			item.age = parseInt(dateObj / 1000 / 60 / 60 / 24 / 365);
+        			let dayNum = d3[0] * (d3[1]-1);
         		}
         		else item.age = null;
-        	} else{
+        	
+        	let dateObj = new Date(today.getFullYear(), d3[1]-1, d3[0]);
+        	item.delta = parseInt((dateObj - today) / 1000 / 60 / 60 / 24);
+
+        	} else {
         		item.age = null;
+        		item.delta = null;
         	}
-        	//console.log(item.last_name, item.age);
         }
-        trList.innerHTML = shabFun({list: response.response});
+
+        // сортировка по дате
+        var minInd, j = 0;
+        for(let a = 1; a>-2 ; a-=2){
+	        while(len){
+	        	for(var i=0, min = 999; i<len; i++){
+	        		let dlt = response.response[i].delta;
+	        		if(a>0 && dlt<0 || a<0 && dlt>=0 || dlt == null) continue;
+	        		if(dlt < min){
+	        			min = dlt;
+	        			minInd = i;
+	        		}
+	        	}
+	        	if(min == 999) break;
+	        	friends[j++]=response.response[minInd];
+	    		response.response.splice(minInd, 1);
+	    		len--;
+	    	}
+    	}
+        //trList.innerHTML = shabFun({list: response.response});
+        trList.innerHTML = shabFun({list: friends});
         resolve();
       }
     });
   })
 });
+
