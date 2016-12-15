7f607b28c1f5a6cad9aa0165bd2a4668b352e7d6
ETi <avalokitesvara@ya.ru>
2016-12-15 13:30:05 +0300

    переделал калькулятор в класс

--- dz9/02/9-2.js
+++ dz9/02/9-2.js
@@ -1,29 +1,29 @@
 var calculator = function(firstNumber){
-    var ob = {
-        sum: function(){
+        this.sum = function(){
             for(var i= 0, res = firstNumber; i<arguments.length; i++) res+=arguments[i];
             return res;
         },
-        dif: function(){
+        this.dif = function(){
             for(var i= 0, res = firstNumber; i<arguments.length; i++) res-=arguments[i];
             return res;
         },
-        div: function(){
+        this.div = function(){
             for(var i= 0, res = firstNumber; i<arguments.length; i++){
                 if(!arguments[i]) throw new Error('операция не может быть произведена, т.к. делитель равен нулю');
                 res/=arguments[i];
             }
             return res;
         },
-        mul: function(){
+        this.mul = function(){
             for(var i= 0, res = firstNumber; i<arguments.length; i++) res*=arguments[i];
             return res;
         }
-    };
-    return ob;
 }
 
-var myCalculator = calculator(100);
+var myCalculator = new calculator(100);
+console.log(myCalculator);
+
+//var myCalculator = calculator(100);
 console.log(myCalculator.sum(1, 2, 3)); // 1, 2, 3 - вернет 106
 console.log(myCalculator.dif(10, 20)); // 10, 20 - вернет 70
 console.log(myCalculator.mul(2, 2)); // 2, 2 - вернет 400
