acad087444eaf05c80fe1c71aeea1e4999a26a9b
eti15 <avalokitesvara@ya.ru>
2016-12-25 11:17:12 +0200

    methodes to parent class

--- dz9/02/9-2.js
+++ dz9/02/9-2.js
@@ -23,7 +23,20 @@ calculator.prototype.div = function(){
 
 var SqrCalc = function(firstNumber){
     this.firstNumber = firstNumber;
-    //this.sum = function(){ return Math.pow(super.sum.apply(SqrCalc,arguments), 2);}
+    this.sum = function(){
+        return Math.pow(this.__proto__.sum.apply(this,arguments), 2);
+    }
+    this.dif = function(){
+        return Math.pow(this.__proto__.dif.apply(this,arguments), 2);
+    }
+
+    this.mul = function(){
+        return Math.pow(this.__proto__.mul.apply(this,arguments), 2);
+    }
+
+    this.div = function(){
+        return Math.pow(this.__proto__.div.apply(this,arguments), 2);
+    }
 }
 
 // наследование (ES5)
@@ -48,7 +61,7 @@ var myCalculator = new SqrCalc(100);
 // стрелочная функция не работает (Uncaught TypeError: Cannot read property 'apply' of undefined)
 // myCalculator.sum = () => Math.pow(this.__proto__.sum.apply(this,arguments), 2);
 
-myCalculator.sum = function(){
+/*myCalculator.sum = function(){
     return Math.pow(this.__proto__.sum.apply(this,arguments), 2);
 }
 
@@ -62,7 +75,7 @@ myCalculator.mul = function(){
 
 myCalculator.div = function(){
     return Math.pow(this.__proto__.div.apply(this,arguments), 2);
-}
+}*/
 
 console.log(myCalculator.sum(1, 2, 3)); // 1, 2, 3 - вернет 106
 console.log(myCalculator.dif(10, 20)); // 10, 20 - вернет 70
